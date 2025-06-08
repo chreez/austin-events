e945c7db59500f02307c434d18f29de60cf368d2
chreez <chreez@gmail.com>
2025-06-07 22:46:21 -0500

    Filter unsold events and redirect root

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Link from 'next/link';
-import Hero from '../components/Hero';
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: '/events',
+      permanent: false
+    }
+  };
+}
 
 export default function Home() {
-  return (
-    <>
-      <Hero>
-        <Link href="/events" className="bg-hotpink text-limestone px-6 py-3 rounded shadow-md hover:bg-turquoise transition-colors">Browse Events</Link>
-      </Hero>
-    </>
-  );
+  return null;
 }
